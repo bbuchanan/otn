4dc59236e2c00291ea2529411d7f1d5ca988e738
Bill Buchanan <bbuchanan@gmail.com>
2018-11-27 15:37:57 -0500

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
+  load = () => {
+    axios
+      .get("https://query2.finance.yahoo.com/v7/finance/options/amd")
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.load}>Click me</button>
         </header>
       </div>
     );
