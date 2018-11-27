d2a4423f16c2c013f469a43fbaf9fe87827023b7
Bill Buchanan <bbuchanan@gmail.com>
2018-11-27 15:48:22 -0500

    Updates

---
+++ precache-manifest.dd99cb9713b45f2fbd0d4faba0b5a267.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b06646ccdf2d4d41d36",
+    "url": "/otn/static/css/main.f2ff85fa.chunk.css"
+  },
+  {
+    "revision": "3b06646ccdf2d4d41d36",
+    "url": "/otn/static/js/main.3b06646c.chunk.js"
+  },
+  {
+    "revision": "a6f98195d10f661a5f96",
+    "url": "/otn/static/js/1.a6f98195.chunk.js"
+  },
+  {
+    "revision": "53ad9b43cea02ffd2f8a",
+    "url": "/otn/static/js/runtime~main.53ad9b43.js"
+  },
+  {
+    "revision": "4a17c77a9f2ceaf38657e3a5629f1742",
+    "url": "/otn/index.html"
+  }
+];
